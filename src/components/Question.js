52180880bcb5ed9db1b41d8292600ce50a75bf46
brianwaithaka <waithakabrian8@gmail.com>
2025-11-16 16:41:27 +0300

    complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    const callBack = setTimeout(() => {
+      setTimeRemaining((curr) => curr - 1);
+    }, 1000);
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+    return function cleanUp() {
+      clearTimeout(callBack);
+    };
+  }, [onAnswered, timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
